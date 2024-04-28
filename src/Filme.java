4c550fd1c997fa92e8906e8fc4095dbd89d35cde
Allyson Araujo <allyson.m.araujo@gmail.com>
2024-04-27 21:11:09 -0300

    encapsulamento incluido

--- src/Filme.java
+++ src/Filme.java
@@ -2,15 +2,24 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes(){
+        return totalDeAvaliacoes;
+    }
+
+    double getSomaDasAvaliacoes(){
+        return somaDasAvaliacoes;
+    }
 
     void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         System.out.println("Duração em minutos: " + duracaoEmMinutos);
         System.out.println("Avaliação: " + somaDasAvaliacoes);
+
     }
 
     void avalia(double nota){
--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,6 @@ public class Principal {
         meuFilme.anoDeLancamento = 1970;
         meuFilme.nome = "O Poderoso Chefão";
         meuFilme.duracaoEmMinutos = 180;
-        meuFilme.somaDasAvaliacoes = 9.6;
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -14,6 +13,10 @@ public class Principal {
         meuFilme.avalia(7.9);
 
 
-        System.out.println("A Media do filme " + meuFilme.nome + " é " + meuFilme.pegaMedia());
+        System.out.println("A Media de notas do filme '" + meuFilme.nome + "' é " + meuFilme.pegaMedia());
+        System.out.println("A soma das avaliações é: " + meuFilme.getSomaDasAvaliacoes());
+        System.out.println("O numero total de avaliações é : " + meuFilme.getTotalDeAvaliacoes());
+
+
     }
 }
